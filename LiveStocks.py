1c066626d9975d4e94f0e59fbacb175f3505eec0
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2020-05-14 12:25:08 -0700

    v 3.0
    - add ui for asking stock and period history
    - add graph for live data of stock price
    - finishing touches

--- LiveStocks.py
+++ LiveStocks.py
@@ -20,107 +20,114 @@ import matplotlib.ticker as mticker
 import matplotlib.animation as animation
 
 
+# getting runtime and memory program used up
+def time_memory():
+    print()
+    print("%s seconds" % round((time.time() - start_time)))
+    print(str(round((process.memory_info().rss)/1000000)) + " MB") # in bytes
 
-fig = plt.figure()
-ax1 = plt.subplot2grid((1, 1), (0, 0))
 
-
-
-def animate(i):
-    stock = yf.Ticker("NVDA")
-
-    hist = stock.history(period="5d", rounding=True)
-    
-    # putting data into csv
-    dateData = hist['Close']
-    dateData.to_csv('date.csv')
-    dateOHLC = []
-    with open('date.csv') as f:
-        reader = csv.reader(f, delimiter=",")
-        next(f)
-        for i in reader:
-            dateOHLC.append(i[0])
-
-    openData = hist['Open']
-    openData.to_csv('open.csv')
-    openOHLC = []
-    with open('open.csv') as f:
-        reader = csv.reader(f, delimiter=",")
-        next(f)
-        for i in reader:
-            openOHLC.append(i[1])
-
-    highData = hist['High']
-    highData.to_csv('high.csv')
-    highOHLC = []
-    with open('high.csv') as f:
-        reader = csv.reader(f, delimiter=",")
-        next(f)
-        for i in reader:
-            highOHLC.append(i[1])
-
-    lowData = hist['Low']
-    lowData.to_csv('low.csv')
-    lowOHLC = []
-    with open('low.csv') as f:
-        reader = csv.reader(f, delimiter=",")
-        next(f)
-        for i in reader:
-            lowOHLC.append(i[1])
-
-    closeData = hist['Close']
-    closeData.to_csv('close.csv')
-    closeOHLC = []
-    with open('close.csv') as f:
-        reader = csv.reader(f, delimiter=",")
-        next(f)
-        for i in reader:
-            closeOHLC.append(i[1])
-
-    #converting prices into int
-    def convertArratInt(array):
-        for i in range(0, len(array)): 
-            array[i] = float(array[i]) 
-    convertArratInt(openOHLC)
-    convertArratInt(highOHLC)
-    convertArratInt(lowOHLC)
-    convertArratInt(closeOHLC)
-
-    # converting date into num
-    newdateOHLC = []
-    def convertDate2Num(array):
-        for i in array:
-            dateTimeOBJ = dt.datetime.strptime(i, '%Y-%m-%d')
-            t1 = dateTimeOBJ.timetuple()
-            newformat = time.mktime(t1)
-            newdateOHLC.append(newformat)
-    convertDate2Num(dateOHLC)
-
-    # appending required data for candlestick chart
-    x = 0
-    y = len(newdateOHLC)
-    ohlc = []
-    while x < y:
-        appendMe = newdateOHLC[x], openOHLC[x], highOHLC[x], lowOHLC[x], closeOHLC[x]
-        ohlc.append(appendMe)
-        x += 1
-
-    ax1.clear()
-    ax1.grid(True)
-    candlestick_ohlc(ax1, ohlc, width=10000, colorup='g', colordown='r')
-
-# ax1.xaxis.set_major_locator(xmajor_locator)
-# for label in ax1.xaxis.get_ticklabels():
-#         print(label)
-# ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
-
+# plot size, labels, and grid
+fig = plt.figure(figsize=(20,10.7))
+ax1 = plt.subplot2grid((4, 1), (2, 0), rowspan=2, colspan=1)
+ax1.grid(False)
 plt.xlabel('Date')
 plt.ylabel('Price')
+ax2 = plt.subplot2grid((4, 1), (0, 0), rowspan=2, colspan=1, sharey=ax1) 
+ax2.grid(True)
 plt.title('Stock Name')
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+
+
+# getting stock information using yfinance library
+stock = yf.Ticker("AAPL")
+hist = stock.history(period="1y", rounding=True)
+
+
+# parsing date data, converting to string and creating array
+dateData = hist.index
+dateOHLC = []
+for i in dateData:
+    i = str(i)
+    dateOHLC.append(i)
+
+# parsing open stock info, creating csv file, adding data to array
+openData = hist['Open']
+openData.to_csv('open.csv')
+openOHLC = []
+with open('open.csv') as f:
+    reader = csv.reader(f, delimiter=",")
+    next(f)
+    for i in reader:
+        openOHLC.append(i[1])
+
+# parsing high stock info, creating csv file, adding data to array
+highData = hist['High']
+highData.to_csv('high.csv')
+highOHLC = []
+with open('high.csv') as f:
+    reader = csv.reader(f, delimiter=",")
+    next(f)
+    for i in reader:
+        highOHLC.append(i[1])
+
+# parsing low stock info, creating csv file, adding data to array
+lowData = hist['Low']
+lowData.to_csv('low.csv')
+lowOHLC = []
+with open('low.csv') as f:
+    reader = csv.reader(f, delimiter=",")
+    next(f)
+    for i in reader:
+        lowOHLC.append(i[1])
+
+# parsing close stock info, creating csv file, adding data to array
+closeData = hist['Close']
+closeData.to_csv('close.csv')
+closeOHLC = []
+with open('close.csv') as f:
+    reader = csv.reader(f, delimiter=",")
+    next(f)
+    for i in reader:
+        closeOHLC.append(i[1])
+
+
+#converting prices into int
+def convertArratInt(array):
+    for i in range(0, len(array)): 
+        array[i] = float(array[i]) 
+convertArratInt(openOHLC)
+convertArratInt(highOHLC)
+convertArratInt(lowOHLC)
+convertArratInt(closeOHLC)
+
+# converting date into correct num format
+def bytespdate2num(b):
+    return mdates.datestr2num(b)
+newdateOHLC = []
+def convertDate2Num(array):
+    for i in array:
+        # dateTimeOBJ = dt.datetime.strptime(i, '%Y-%m-%d')
+        # t1 = dateTimeOBJ.timetuple()
+        newformat = bytespdate2num(i)
+        newdateOHLC.append(newformat)
+convertDate2Num(dateOHLC)
+
+# appending required data for candlestick chart
+x = 0
+y = len(newdateOHLC)
+ohlc = []
+while x < y:
+    appendMe = newdateOHLC[x], openOHLC[x], highOHLC[x], lowOHLC[x], closeOHLC[x]
+    ohlc.append(appendMe)
+    x += 1
+
+
+candlestick_ohlc(ax1, ohlc, width=1./24, colorup='g', colordown='r') # adding data to candlestick chart
+plt.subplots_adjust(left=0.05, bottom=0.11, right=0.9, top=0.95, wspace=0.2, hspace=0) # adjusting window
+for label in ax1.xaxis.get_ticklabels(): # rotating date labels on x axis
+        label.set_rotation(45)
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y')) # converting x-axis to proper date format
+plt.setp(ax2.get_xticklabels(), visible=False) # hiding top graph x axis labels
 plt.show()
 
-# print()
-# print("%s seconds" % (time.time() - start_time))
-# print(str((process.memory_info().rss)/1000000) + " MB") # in bytes
-
+time_memory()
--- testCandlestick.py
+++ testCandlestick.py
@@ -4,28 +4,47 @@ import matplotlib.ticker as mticker
 from mplfinance.original_flavor import candlestick_ohlc
 import numpy as np
 import urllib.request
-import datetime as dt
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+MA1 = 10
+MA2 = 30
+
+def highMinusLow(high, low):
+    return high-low
 
 
-# Using SentDex method instead of pandas: fixed certificates problem
+def movingAverage(value, window):
+    weights = np.repeat(1.0, window)/window
+    smas = np.convolve(value, weights, 'valid')
+    return smas
+
 
 def bytespdate2num(b):
     return mdates.datestr2num(b.decode('utf-8'))
 
 
 def graph_data(days):
-
     fig = plt.figure()
-    ax1 = plt.subplot2grid((1, 1), (0, 0))
+    ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=1, colspan=1)
+    plt.title('Stock Info')
+    plt.ylabel('H-L')
+    ax2 = plt.subplot2grid((6, 1), (1, 0), rowspan=4, colspan=1, sharex=ax1)
+    # plt.xlabel('Date')
+    plt.ylabel('Price')
+    ax2v = ax2.twinx() # truly share that x-axis
+    ax3 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+    plt.ylabel('MovingAvgs')
 
     stockPrice = 'https://pythonprogramming.net/yahoo_finance_replacement'
     sourceCode = urllib.request.urlopen(stockPrice).read().decode()
     stockData = []
     splitSource = sourceCode.split('\n')
 
-    stDays = int(days)
+    intDays = int(days)
 
-    for line in splitSource[1:(stDays+1)]:
+    for line in splitSource[1:(intDays+1)]:
         splitLine = line.split(',')
         if len(splitLine) == 7:
             if 'values' not in line and 'labels' not in line:
@@ -33,7 +52,7 @@ def graph_data(days):
 
     date, closep, highp, lowp, openp, adj_closep, volume = np.loadtxt(stockData, delimiter=',', unpack=True,
                                                                       converters={0: bytespdate2num})
-
+    print(date)
     x = 0
     y = len(date)
     ohlc = []
@@ -43,22 +62,51 @@ def graph_data(days):
         ohlc.append(appendMe)
         x += 1
 
-    candlestick_ohlc(ax1, ohlc, width=0.4, colorup='g', colordown='r')
+    ma1 = movingAverage(closep, MA1)
+    ma2 = movingAverage(closep, MA2)
+    start = len(date[MA2-1:])
 
-    for label in ax1.xaxis.get_ticklabels():
-        # label.set_rotation(45)
-        print(label)
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    # ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
-    ax1.grid(True)
+    h_l = list(map(highMinusLow, highp, lowp))
 
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title('stock')
-    # plt.legend()
-    plt.subplots_adjust(left=0.09, bottom=0.20, right=0.94, top=0.90, wspace=0.2, hspace=0)
-    plt.show()
+    ax1.plot_date(date[-start:], h_l[-start:], '-', label='H-L')
+    ax1.yaxis.set_major_locator(mticker.MaxNLocator(nbins=4, prune='lower'))
+
+    candlestick_ohlc(ax2, ohlc[-start:], width=0.4, colorup='g', colordown='r')
+
+    ax2.yaxis.set_major_locator(mticker.MaxNLocator(nbins=7, prune='upper'))
+    ax2.grid(True)
+
+    bbox_props = dict(boxstyle='round', fc='w', ec='k', lw=1)  # adding annotation for last price
+    ax2.annotate(str(closep[0]), (date[0], closep[0]), xytext=(date[-start] + 4, closep[-start]), bbox=bbox_props, fontsize='xx-small')
+    ax2v.plot([], [], color='#007983', alpha=0.4, label='Volume')
+    ax2v.fill_between(date[-start:], 0, volume[-start:], facecolor='#007983', alpha=0.4)
+    ax2v.axes.yaxis.set_ticklabels([])
+    ax2v.grid(False)
+    ax2v.set_ylim(0, 3*volume.max())
+
+    ax3.plot(date[-start:], ma1[-start:], linewidth=3, label=str(MA1)+'MA1')
+    ax3.plot(date[-start:], ma2[-start:], linewidth=3, label=str(MA2)+'MA2')
+    ax3.fill_between(date[-start:], ma2[-start:], ma1[-start:], where=(ma1[-start:] < ma2[-start:]), facecolor='r', edgecolor='r', alpha=0.5)
+    ax3.fill_between(date[-start:], ma2[-start:], ma1[-start:], where=(ma1[-start:] > ma2[-start:]), facecolor='g', edgecolor='g', alpha=0.5)
+    ax3.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
+    ax3.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    ax3.yaxis.set_major_locator(mticker.MaxNLocator(nbins=4, prune='upper'))
+    for label in ax3.xaxis.get_ticklabels():
+        label.set_rotation(45)
     
 
-days = input('How many days of stock history?: ')
-graph_data(days)
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.subplots_adjust(left=0.11, bottom=0.24, right=0.9, top=0.90, wspace=0.2, hspace=0)
+    ax1.legend()
+    leg = ax1.legend(loc=9, ncol=2, prop={'size':11})
+    leg.get_frame().set_alpha(0.4)
+    ax2v.legend()
+    leg = ax2v.legend(loc=9, ncol=2, prop={'size':11})
+    leg.get_frame().set_alpha(0.4)
+    ax3.legend()
+    leg = ax3.legend(loc=9, ncol=2, prop={'size':11})
+    leg.get_frame().set_alpha(0.4)
+    plt.show()
+
+graph_data(days=input('How many days of stock history?: '))
