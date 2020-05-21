515ae9725e38946f156772e05bc54a46d42339d3
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2020-05-20 21:06:27 -0700

    Update LiveStocks.py

--- LiveStocks.py
+++ LiveStocks.py
@@ -28,7 +28,7 @@ stockPeriod = input('Stock History (Options: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 
 
 
 # plot size, labels, grid, and different axes
-fig = plt.figure(figsize=(20,10.7)) # size for bigger screens (20, 10.7)
+fig = plt.figure(figsize=(20,10.7))
 ax1 = plt.subplot2grid((4, 1), (2, 0), rowspan=2, colspan=1)
 ax1.grid(False)
 plt.xlabel('Date')
@@ -41,38 +41,40 @@ ax2l = plt.subplot2grid((4, 1), (0, 0), rowspan=2, colspan=1)
 stock = yf.Ticker(stockName)
 hist = stock.history(period=stockPeriod, rounding=True)
 
-
-# parsing date data, converting to string and creating array
-dateData = hist.index
-dateOHLC = []
-for i in dateData:
-    i = str(i)
-    dateOHLC.append(i)
+dateOHLC = None 
+openOHLC = None
+highOHLC = None
+lowOHLC = None
+closeOHLC = None
 
 
-# parsing open stock info and adding data to array
-openOHLC = hist["Open"].tolist()
+def parseData():
+    global dateOHLC
+    global openOHLC
+    global highOHLC
+    global lowOHLC
+    global closeOHLC
+    dateData = hist.index
+    dateOHLC = []
+    for i in dateData:
+        i = str(i)
+        dateOHLC.append(i)
+    openOHLC = hist["Open"].tolist()
+    highOHLC = hist["High"].tolist()
+    lowOHLC = hist["Low"].tolist()
+    closeOHLC = hist["Close"].tolist()
 
-# parsing high stock info and adding data to array
-highOHLC = hist["High"].tolist()
-
-# parsing low stock info and adding data to array
-lowOHLC = hist["Low"].tolist()
-
-# parsing close stock info and adding data to array
-closeOHLC = hist["Close"].tolist()
+parseData()
 
 
-#converting prices into int
+#converting data
 def convertArratInt(array):
     for i in range(0, len(array)): 
-        array[i] = float(array[i]) 
-    convertArratInt(openOHLC)
-    convertArratInt(highOHLC)
-    convertArratInt(lowOHLC)
-    convertArratInt(closeOHLC)
-
-# converting date into correct num format
+        array[i] = float(array[i])       
+convertArratInt(openOHLC)
+convertArratInt(highOHLC)
+convertArratInt(lowOHLC)
+convertArratInt(closeOHLC)
 def bytespdate2num(b):
     return mdates.datestr2num(b)
 newdateOHLC = []
@@ -103,11 +105,10 @@ def animate(i):
     plt.title(stockName.upper())
     ax2.plot()
     
-
-
 candlestick_ohlc(ax1, ohlc, width=1./24, colorup='g', colordown='r') # adding data to candlestick chart
 ax2l.plot()
 ani = animation.FuncAnimation(fig, animate, interval=500) # updating the first graph through animation
+
 plt.subplots_adjust(left=0.05, bottom=0.11, right=0.9, top=0.95, wspace=0.2, hspace=0.73) # adjusting window
 for label in ax1.xaxis.get_ticklabels(): # rotating date labels on x axis
         label.set_rotation(45)
